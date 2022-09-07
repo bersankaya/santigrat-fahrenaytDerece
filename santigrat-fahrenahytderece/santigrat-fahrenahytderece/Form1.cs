b549ff4c2ad86a5f1fa2c6bffd468a9cb21d1fd4
berşan <93613612+bersankaya@users.noreply.github.com>
2022-09-07 23:57:22 +0300

    Santigrat-FahrenaytDonusturme

---
+++ santigrat-fahrenahytderece/santigrat-fahrenahytderece/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace santigrat_fahrenahytderece
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double c = Convert.ToDouble(textBox1.Text);
+            textBox2.Text = Convert.ToString((c *1.8) +32);
+        }
+    }
+}
